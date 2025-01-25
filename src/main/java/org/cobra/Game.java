298247bc58f4751156d9df6bf1693346991b30c8
Mateusz Biel <matbiel@gmail.com>
2025-01-25 21:50:26 +0100

    Draw menu or game area

--- src/main/java/org/cobra/Game.java
+++ src/main/java/org/cobra/Game.java
@@ -19,6 +19,7 @@ public class Game implements KeyListener {
   private final int windowHeight;
   private final Random random = new Random();
   private float appleTime;
+  private GamePage gamePage;
 
   public Game(MainMenu mainMenu, GameArea gameArea, int windowWidth, int windowHeight) {
     this.mainMenu = mainMenu;
@@ -26,6 +27,7 @@ public class Game implements KeyListener {
     this.windowWidth = windowWidth;
     this.windowHeight = windowHeight;
     appleTime = 20f;
+    gamePage = GamePage.MENU;
   }
 
   @Override
@@ -57,6 +59,13 @@ public class Game implements KeyListener {
             if (event.keyCode() == KeyEvent.VK_S) {
               velocityY = 30.0f;
             }
+            if (event.keyCode() == KeyEvent.VK_ESCAPE) {
+              if (GamePage.MENU == gamePage) {
+                gamePage = GamePage.GAME;
+              } else {
+                gamePage = GamePage.MENU;
+              }
+            }
           }
           if (event.keyAction == KeyAction.RELEASED) {
             if (event.keyCode() == KeyEvent.VK_A || event.keyCode() == KeyEvent.VK_D) {
@@ -68,22 +77,27 @@ public class Game implements KeyListener {
           }
         });
     keyEvents.clear();
-    positionX += velocityX * delta;
-    positionY += velocityY * delta;
-    if (appleTime >= 20f) {
-      gameArea.setPosition(new GameArea.Position(random.nextInt(16), random.nextInt(16)));
-      appleTime = 0f;
+    if (GamePage.GAME == gamePage) {
+      positionX += velocityX * delta;
+      positionY += velocityY * delta;
+      if (appleTime >= 20f) {
+        gameArea.setPosition(new GameArea.Position(random.nextInt(16), random.nextInt(16)));
+        appleTime = 0f;
+      }
+      appleTime += delta;
     }
-    appleTime += delta;
   }
 
   public void draw(Graphics2D graphics) {
-    // mainMenu.update();
-    // graphics.drawImage(mainMenu, 0, 0, null);
-    gameArea.update();
-    int topPadding = (windowHeight - gameArea.getHeight()) >> 1;
-    int leftPadding = (windowWidth - gameArea.getWidth()) >> 1;
-    graphics.drawImage(gameArea, null, leftPadding, topPadding);
+    if (gamePage == GamePage.MENU) {
+      mainMenu.update();
+      graphics.drawImage(mainMenu, 0, 0, null);
+    } else if (gamePage == GamePage.GAME) {
+      gameArea.update();
+      int topPadding = (windowHeight - gameArea.getHeight()) >> 1;
+      int leftPadding = (windowWidth - gameArea.getWidth()) >> 1;
+      graphics.drawImage(gameArea, null, leftPadding, topPadding);
+    }
   }
 
   private enum KeyAction {
@@ -92,4 +106,9 @@ public class Game implements KeyListener {
   }
 
   private record GameKeyEvent(int keyCode, KeyAction keyAction) {}
+
+  private enum GamePage {
+    GAME,
+    MENU,
+  }
 }
